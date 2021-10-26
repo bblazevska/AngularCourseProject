b6c9280ae210e748541efb8f828be74001ded384
kuzeva <kuzeva01@gmail.com>
2021-10-26 16:32:18 +0200

    passing ingredients from recipe to shopping-list

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,23 +1,38 @@
-import { EventEmitter } from "@angular/core";
+import { EventEmitter, Injectable } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
 import { Recipe } from "./recipe.model";
 
-
+@Injectable()
 export class RecipeService {
-   recipeSelected = new EventEmitter<Recipe>();
-   private recipes: Recipe[] = [
+  recipeSelected = new EventEmitter<Recipe>();
+  private recipes: Recipe[] = [
     new Recipe(
       'A test Recipe',
       'This is simply a test',
-      'https://assets.bonappetit.com/photos/60a4022a248102a01bcfa0b6/1:1/w_2560%2Cc_limit/0621-Sheet-Pan-Gnocchi.jpg'
+      'https://assets.bonappetit.com/photos/60a4022a248102a01bcfa0b6/1:1/w_2560%2Cc_limit/0621-Sheet-Pan-Gnocchi.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]
     ),
     new Recipe(
       'Another test Recipe',
       'This is simply another test',
-      'https://assets.bonappetit.com/photos/60a4022a248102a01bcfa0b6/1:1/w_2560%2Cc_limit/0621-Sheet-Pan-Gnocchi.jpg'
+      'https://assets.bonappetit.com/photos/60a4022a248102a01bcfa0b6/1:1/w_2560%2Cc_limit/0621-Sheet-Pan-Gnocchi.jpg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1)
+      ]
     ),
-   ];
+  ];
 
-   getRecipes() {
-      return this.recipes.slice();
-   }
+  constructor(private slService: ShoppingListService){}
+  getRecipes() {
+    return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
+  }
 }
