c0dfcfa49d713fbaf28e74b1fa686a440a9f8c7f
Bojana Kuzeva ATIS <bojana.kuzeva@atis.al>
2021-11-02 14:54:38 +0100

    hanling errors + loading spinner added

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -9,6 +9,8 @@ import { AuthService } from './auth.service';
 })
 export class AuthComponent implements OnInit {
   isLoginMode = true;
+  isLoading = false;
+  error: string = null;
 
   constructor(private authService: AuthService) { }
 
@@ -24,15 +26,17 @@ export class AuthComponent implements OnInit {
     const email = form.value.email;
     const password = form.value.password;
 
+    this.isLoading = true;
     if (this.isLoginMode) {
       // ...
     } else {
-      
       this.authService.signup(email, password).subscribe(
         resData => {
           console.log(resData);
-        }, error => {
-          console.log(error);
+          this.isLoading = false;
+        }, errorMessage => {
+          this.error = errorMessage;
+          this.isLoading = false;
         }
       );
         
