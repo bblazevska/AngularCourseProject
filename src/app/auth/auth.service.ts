7694f07e836d0affd3fa89c7a3157bbe2bc9f70d
Bojana Kuzeva ATIS <bojana.kuzeva@atis.al>
2021-11-02 16:24:25 +0100

    logout added

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from "@angular/core";
 import { catchError, tap } from "rxjs/operators";
 import { BehaviorSubject, throwError } from "rxjs";
 import { User } from "./user.model";
+import { Router } from "@angular/router";
 
 export interface AuthResponseData{
    kind: string,
@@ -20,7 +21,7 @@ export class AuthService {
    user = new BehaviorSubject<User>(null);
    
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private router: Router) {}
 
   signup(email: string, password: string) {
     return this.http
@@ -65,9 +66,14 @@ export class AuthService {
             +resData.expiresIn
           );
         })
-      );
+     );
    }
    
+   logout() {
+      this.user.next(null);
+      this.router.navigate(['/auth']);
+   }
+
    private handleAuthentication(email: string,userId: string, token: string, expiresIn: number) {
       const exparationDate = new Date(new Date().getTime() + expiresIn * 1000 );
 
